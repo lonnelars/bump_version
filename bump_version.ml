6cba4ab8b0dcb1e5d37f11dab95ba54d3d7c2097
Lars Lønne <lars.lonne@kantega.no>
2019-02-12 09:08:32 +0100

    Read command as arg, and not from stdin

--- bump_version.ml
+++ bump_version.ml
@@ -22,17 +22,19 @@ let parse_command s =
 
 let print_usage () =
   print_endline
-    "Usage: bump_version expects one command on stdin. The command must be \
-     major, minor, or patch. It will read the current version from the file \
-     named version, and write the new version to the same file."
+    "Usage: bump_version expects one command. The command must be major, \
+     minor, or patch. It will read the current version from the file named \
+     version, and write the new version to the same file."
 
 let print_version major minor patch =
   let new_version = sprintf "%i.%i.%i\n" major minor patch in
   Out_channel.write_all "version" ~data:new_version
 
-let () =
+let command_param = Command.Param.(anon ("command" %: string))
+
+let main command =
   let open Option in
-  let command = In_channel.input_line In_channel.stdin >>= parse_command in
+  let command = parse_command command in
   let version =
     In_channel.input_line (In_channel.create "version") >>= parse_version
   in
@@ -42,3 +44,9 @@ let () =
   | Some (major, minor, _), Some Minor -> print_version major (minor + 1) 0
   | Some (major, minor, patch), Some Patch ->
       print_version major minor (patch + 1)
+
+let command =
+  Command.basic ~summary:"Increase version number in file `version`"
+    (Command.Param.map command_param ~f:(fun command () -> main command))
+
+let () = Command.run command
